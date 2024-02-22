328e00b50adfaa35f3e4d8b109b87fc2e5d2b22b
João Pedro Paranhos de Farias <111139042+J040P@users.noreply.github.com>
2024-02-22 18:59:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,24 +3,27 @@ const navi = document.querySelector(".menu")
 
 const bts = document.querySelectorAll(".btn-slides")
 const slides = document.querySelectorAll(".video1")
+const contents = document.querySelectorAll(".content")
 
 menubtn.addEventListener("click",()=>{
     menubtn.classList.toggle("active");
     navi.classList.toggle("active");    
 })
 
-
-
 var slidernav = function(manual){
     bts.forEach((btn)=>{
         btn.classList.remove("active")
     })
-    bts.forEach((slide)=>{
-        slide.classList.remove("active")
+    slides.forEach((slid)=>{
+        slid.classList.remove("active")
+    })
+    contents.forEach((legenda)=>{
+        legenda.classList.remove("active")
     })
 
     bts[manual].classList.add("active")
     slides[manual].classList.add("active")
+    contents[manual].classList.add("active")
 }
 
 bts.forEach((btn,i)=>{
